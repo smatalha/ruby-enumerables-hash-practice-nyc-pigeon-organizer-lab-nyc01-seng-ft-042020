13e31ce2fa22cda8bfa222421894e4b030c33081
Md Abu Talha Mian <sma.talha@yahoo.com>
2020-04-14 21:30:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,22 @@
 def nyc_pigeon_organizer(data)
   new_hash = {}
-  
-  data.each { |k, v| new_hash[v] = k}
+  data.each do |k, v|
+    # p value
+    v.each do |value, names|
+      #p value
+      names.each do |name|
+      # p names
+        if !new_hash[name]
+          new_hash[name] = {}
+        end
+        if !new_hash[name][k]
+          new_hash[name][k] = []
+        end
+
+        new_hash[name][k] << value.to_s
+
+      end
+    end
+  end
+  new_hash
 end
